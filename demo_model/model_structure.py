3fe95c179e917946d7743c9ee82bbd909a581923
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-16 12:41:53 +0000

    Test

--- demo_model/model_structure.py
+++ demo_model/model_structure.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 def render_png_image():
-    image_path = './images/De-Biasing Diffusion Model.png'
-    st.image(image_path)
+    
+    return
 
 # Update the ST_MODEL_STRUCTURE_DEMOS dictionary
 ST_MODEL_STRUCTURE_DEMOS = {
-    "Display PNG Image": (
+    "Display Model Structure": (
         render_png_image,
         None,
     ),
