8d8ec72ea5959a3316c192ac52c8a90d598b4f3b
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-17 05:46:10 +0000

    Modify the layout of dataset 1 experiment

--- demo_experiments/stack_bar.py
+++ demo_experiments/stack_bar.py
@@ -7,23 +7,23 @@ from streamlit_echarts import st_pyecharts
 def dataset1():
     b = (
         Bar()
-        .add_xaxis(["0", "0.01", "0.05", "0.1"])
-        .add_yaxis("Male", [0.4, 0.4, 0.4, 0.4], stack="stack1", color='blue')
-        .add_yaxis("Female", [0.6, 0.6, 0.6, 0.6], stack="stack1", color='pink')
+        .add_xaxis(["0", "0.01", "0.05", "0.1"])  # Alpha values as categories
+        .add_yaxis("Male", [0.4, 0.4, 0.4, 0.4], stack="stack1", color='blue')  # Male proportions, stacked
+        .add_yaxis("Female", [0.6, 0.6, 0.6, 0.6], stack="stack1", color='pink')  # Female proportions, stacked
         .set_global_opts(
             title_opts=opts.TitleOpts(
                 title="Male and Female Proportions at Different Alpha Values",
                 subtitle="A stacked bar chart",
-                pos_top="5%",  # Adjust the position of the title to give more space
+                pos_top="10%",  # Increase this value to move the title higher
             ),
             legend_opts=opts.LegendOpts(
-                pos_top="15%",  # Adjust the position of the legend to move it below the title
+                pos_top="15%",  # Increase this value to move the legend below the title
             ),
             yaxis_opts=opts.AxisOpts(
-                name_gap=30,  # Adjust the gap between the y-axis name and the axis line
+                name_gap=40,  # Adjust the gap between the y-axis name and the axis line
                 axislabel_opts=opts.LabelOpts(
                     font_size=14,
-                    margin=20,  # Adjust the margin to ensure labels don't overlap with axis line
+                    margin=15,  # Adjust the margin to ensure labels don't overlap with axis line
                 ),
             ),
         )
