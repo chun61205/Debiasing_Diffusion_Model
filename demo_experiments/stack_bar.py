81604c868ece116d279e111cf816a21c13ce5ede
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-17 05:54:40 +0000

    MoCorrecthebarchart propotion

--- demo_experiments/stack_bar.py
+++ demo_experiments/stack_bar.py
@@ -8,8 +8,8 @@ def dataset1():
     b = (
         Bar()
         .add_xaxis(["0", "0.01", "0.05", "0.1"])  # Alpha values as categories
-        .add_yaxis("Male", [0.4, 0.4, 0.4, 0.4], stack="stack1", color='blue')  # Male proportions, stacked
-        .add_yaxis("Female", [0.6, 0.6, 0.6, 0.6], stack="stack1", color='pink')  # Female proportions, stacked
+        .add_yaxis("Male", [0.36, 0.39, 0.58, 0.6], stack="stack1", color='blue')  # Male proportions, stacked
+        .add_yaxis("Female", [0.64, 0.61, 0.42, 0.4], stack="stack1", color='pink')  # Female proportions, stacked
         .set_global_opts(
             title_opts=opts.TitleOpts(
                 title="Male and Female Proportions at Different Alpha Values",
@@ -30,6 +30,8 @@ def dataset1():
     st_pyecharts(
         b, key="echarts"
     )  # Add key argument to not remount component at every Streamlit run
+
+
 
 ST_STACKBAR_DEMOS = {
     "Male : Female = 1 : 4": (
