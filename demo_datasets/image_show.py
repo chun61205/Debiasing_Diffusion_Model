cb6b68d73944d7acb95407d21d301f9d5d90dc57
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-19 03:49:02 +0000

    Add rrefresh button for dataset image show

--- demo_datasets/image_show.py
+++ demo_datasets/image_show.py
@@ -3,20 +3,27 @@ import random
 
 import streamlit as st
 
-# Male : Female = 1 : 4
-def dataset1():
-    dataset_path = './demo_datasets/images/dataset1'
+# Helper function to get a random image
+def get_random_image(dataset_path):
     images = [file for file in os.listdir(dataset_path) if file.endswith('.jpg')]
     selected_image = random.choice(images) if images else None
-    image_path = os.path.join(dataset_path, selected_image) if selected_image else None
-    st.image(image_path)
+    return os.path.join(dataset_path, selected_image) if selected_image else None
 
+# Dataset 1 function with a button to randomize the image
+def dataset1():
+    st.header("Male : Female = 1 : 4")
+    dataset_path = './demo_datasets/images/dataset1'
+    if 'dataset1_image' not in st.session_state or st.button('Randomize Image', key='randomize1'):
+        st.session_state['dataset1_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset1_image'])
+
+# Dataset 2 function with a button to randomize the image
 def dataset2():
+    st.header("Male : Female = 4 : 1")
     dataset_path = './demo_datasets/images/dataset2'
-    images = [file for file in os.listdir(dataset_path) if file.endswith('.jpg')]
-    selected_image = random.choice(images) if images else None
-    image_path = os.path.join(dataset_path, selected_image) if selected_image else None
-    st.image(image_path)
+    if 'dataset2_image' not in st.session_state or st.button('Randomize Image', key='randomize2'):
+        st.session_state['dataset2_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset2_image'])
 
 ST_IMAGE_SHOW_DEMOS = {
     "Male : Female = 1 : 4": (
