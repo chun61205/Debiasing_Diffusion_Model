4d7b5265820563c2dd275ea9b617b5c51b35a44c
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-19 04:07:52 +0000

    Add credit for dataset image show

--- demo_datasets/image_show.py
+++ demo_datasets/image_show.py
@@ -28,10 +28,10 @@ def dataset2():
 ST_IMAGE_SHOW_DEMOS = {
     "Male : Female = 1 : 4": (
         dataset1,
-        None,
+        st.markdown("[CelebAMask-HQ](https://github.com/switchablenorms/CelebAMask-HQ)"),
     ),
     "Male : Female = 4 : 1": (
         dataset2,
-        None,
+        st.markdown("[CelebAMask-HQ](https://github.com/switchablenorms/CelebAMask-HQ)"),
     )
 }
