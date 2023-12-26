2276af4cf41a85f2e4748e24b30199f596f1c977
Chun <89193387+chun61205@users.noreply.github.com>
2023-12-26 03:23:01 +0000

    Add dataset 1 examples

--- demo_examples/image_show.py
+++ demo_examples/image_show.py
@@ -9,29 +9,53 @@ def get_random_image(dataset_path):
     selected_image = random.choice(images) if images else None
     return os.path.join(dataset_path, selected_image) if selected_image else None
 
-# Dataset 1 function with a button to randomize the image
-def dataset1():
-    st.header("Male : Female = 1 : 4")
-    dataset_path = './demo_datasets/images/dataset1'
-    if st.button('Refresh', key='refresh1') or 'dataset1_image' not in st.session_state:
-        st.session_state['dataset1_image'] = get_random_image(dataset_path)
-    st.image(st.session_state['dataset1_image'])
+# Dataset 1 function with a button to randomize the image (alpha = 0)
+def dataset1_0():
+    st.header("Male : Female = 1 : 4, alpha = 0")
+    dataset_path = './demo_examples/images/dataset1_0'
+    if st.button('Refresh', key='refresh1') or 'dataset1_0_image' not in st.session_state:
+        st.session_state['dataset1_0_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset1_0_image'])
 
-# Dataset 2 function with a button to randomize the image
-def dataset2():
-    st.header("Male : Female = 4 : 1")
-    dataset_path = './demo_datasets/images/dataset2'
-    if st.button('Refresh', key='refresh2') or 'dataset2_image' not in st.session_state:
-        st.session_state['dataset2_image'] = get_random_image(dataset_path)
-    st.image(st.session_state['dataset2_image'])
+# Dataset 1 function with a button to randomize the image (alpha = 0.1)
+def dataset1_0_1():
+    st.header("Male : Female = 1 : 4, alpha = 0.1")
+    dataset_path = './demo_examples/images/dataset1_0_1'
+    if st.button('Refresh', key='refresh1') or 'dataset1_0_1_image' not in st.session_state:
+        st.session_state['dataset1_0_1_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset1_0_1_image'])
+
+# Dataset 1 function with a button to randomize the image (alpha = 0.05)
+def dataset1_0_0_5():
+    st.header("Male : Female = 1 : 4, alpha = 0.05")
+    dataset_path = './demo_examples/images/dataset1_0_0_5'
+    if st.button('Refresh', key='refresh1') or 'dataset1_0_0_5_image' not in st.session_state:
+        st.session_state['dataset1_0_0_5_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset1_0_0_5_image'])
+
+# Dataset 1 function with a button to randomize the image (alpha = 0.01)
+def dataset1_0_0_1():
+    st.header("Male : Female = 1 : 4, alpha = 0.01")
+    dataset_path = './demo_examples/images/dataset1_0_0_1'
+    if st.button('Refresh', key='refresh1') or 'dataset1_0_0_1_image' not in st.session_state:
+        st.session_state['dataset1_0_0_1_image'] = get_random_image(dataset_path)
+    st.image(st.session_state['dataset1_0_0_1_image'])
 
 ST_IMAGE_SHOW_DEMOS = {
-    "Male : Female = 1 : 4": (
-        dataset1,
+    "Male : Female = 1 : 4, alpha = 0": (
+        dataset1_0,
         "https://github.com/switchablenorms/CelebAMask-HQ",
     ),
-    "Male : Female = 4 : 1": (
-        dataset2,
+    "Male : Female = 1 : 4, alpha = 0.1": (
+        dataset1_0_1,
         "https://github.com/switchablenorms/CelebAMask-HQ",
-    )
+    ),
+    "Male : Female = 1 : 4, alpha = 0.05": (
+        dataset1_0_0_5,
+        "https://github.com/switchablenorms/CelebAMask-HQ",
+    ),
+    "Male : Female = 1 : 4, alpha = 0.01": (
+        dataset1_0_0_1,
+        "https://github.com/switchablenorms/CelebAMask-HQ",
+    ),
 }
