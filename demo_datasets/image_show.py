783ee7386e21aa6eb7caa0c4609395ec6d03c980
Chun <89193387+chun61205@users.noreply.github.com>
2024-03-02 06:36:22 +0000

    Improve the dataset image display

--- demo_datasets/image_show.py
+++ demo_datasets/image_show.py
@@ -5,8 +5,7 @@ import streamlit as st
 
 # Dataset
 def dataset():
-    if st.button('Refresh', key='refresh1') or 'dataset_image' not in st.session_state:
-        st.session_state['dataset_image'] = './demo_datasets/images/datasets.jpg'
+    st.session_state['dataset_image'] = './demo_datasets/images/datasets.jpg'
     st.image(st.session_state['dataset_image'])
 
 ST_IMAGE_SHOW_DEMOS = {
