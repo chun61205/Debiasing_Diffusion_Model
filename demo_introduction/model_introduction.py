80e6cce46619a08fe39082326158c9aa91bcf908
Chun <89193387+chun61205@users.noreply.github.com>
2024-03-19 04:06:53 +0000

    Merge model and value

--- demo_introduction/model_introduction.py
+++ demo_introduction/model_introduction.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def render_value_image():
-    image_path = './demo_model/images/value.jpg'
+    image_path = './demo_introduction/images/value.jpg'
     st.image(image_path)
 
 def render_model_image():
-    image_path = './demo_model/images/debiasing diffusion model.jpg'
+    image_path = './demo_introduction/images/debiasing diffusion model.jpg'
     st.image(image_path)
 
 ST_DISPLAY_INTRODUCTION_DEMOS = {
