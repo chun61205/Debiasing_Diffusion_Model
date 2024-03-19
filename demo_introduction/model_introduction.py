6ce486800b9086088adabd05d1c8396610042799
Chun <89193387+chun61205@users.noreply.github.com>
2024-03-19 04:06:28 +0000

    Merge model and value

--- demo_introduction/model_introduction.py
+++ demo_introduction/model_introduction.py
@@ -8,16 +8,11 @@ def render_model_image():
     image_path = './demo_model/images/debiasing diffusion model.jpg'
     st.image(image_path)
 
-# Update the ST_VALUE_DEMOS dictionary
-ST_VALUE_DEMOS = {
+ST_DISPLAY_INTRODUCTION_DEMOS = {
     "Value": (
         render_value_image,
         None,
     ),
-}
-
-# Update the ST_MODEL_STRUCTURE_DEMOS dictionary
-ST_MODEL_STRUCTURE_DEMOS = {
     "Display Model Structure": (
         render_model_image,
         "Stable Diffusion + feature learning indicator C = fairness generative model",
