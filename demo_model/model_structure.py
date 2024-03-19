39a79770586bbc243753b9caf109dee68567ef10
Chun <89193387+chun61205@users.noreply.github.com>
2024-03-19 03:16:07 +0000

    Adjust the model image size

--- demo_model/model_structure.py
+++ demo_model/model_structure.py
@@ -8,6 +8,6 @@ def render_png_image():
 ST_MODEL_STRUCTURE_DEMOS = {
     "Display Model Structure": (
         render_png_image,
-        "We introduce the debiasing modele including an bias mitigating indicator C operating in the denoising process, which utilizes denoised latent variables as inputs to identify and learn potential attributes. This indicator outputs a value ŷ which serves as a key factor in the loss computation, thereby guiding the model’s learning process. However, this output is solely utilized for training and is not involved in image generation tasks.",
+        "Stable Diffusion + feature learning indicator = fairness generative model",
     ),
 }
