79858f38f16aee0afcc2696054ccdcfc08bfc2fe
Chun <89193387+chun61205@users.noreply.github.com>
2024-04-08 16:06:58 +0000

    Modify demo

--- demo_demos/image_gen.py
+++ demo_demos/image_gen.py
@@ -12,16 +12,12 @@ def get_random_image(dataset_path):
 # Dataset 1 function with a button to randomize the image (alpha = 0)
 def dataset1_0():
     st.header("Male : Female = 1 : 4, alpha = 0")
-    dataset_path = './demo_examples/images/dataset1_0'
+    dataset_path = './demo_demos/images/dataset1_0'
     if st.button('Refresh', key='refresh1_0') or 'dataset1_0_image' not in st.session_state:
         st.session_state['dataset1_0_image'] = get_random_image(dataset_path)
     st.image(st.session_state['dataset1_0_image'])
 
 ST_IMAGE_GEN_DEMOS = {
-    "Male : Female = 1 : 4, alpha = 0": (
-        dataset1_0,
-        " ",
-    ),
     "Male : Female = 1 : 4, alpha = 0": (
         dataset1_0,
         " ",
