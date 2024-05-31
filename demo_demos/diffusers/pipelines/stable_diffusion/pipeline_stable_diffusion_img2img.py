150a53c76596dad24c37dd1434d9371c1a6ae85f
Chun <roger20020122@gmail.com>
2024-05-31 17:19:00 +0800

    test

--- demo_demos/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
+++ demo_demos/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
@@ -708,7 +708,7 @@ class StableDiffusionImg2ImgPipeline(
                 # expand the latents if we are doing classifier free guidance
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
-                print(latent_model_input.dtype)
+                print(latent_model_input.shape)
                 print(self.unet.device)
                 # predict the noise residual
                 noise_pred = self.unet(
