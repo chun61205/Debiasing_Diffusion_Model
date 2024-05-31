21257f9b05b3633e6e1f49afa10a3e8d6f26eab6
Chun <roger20020122@gmail.com>
2024-05-31 16:55:21 +0800

    test

--- demo_demos/inference.py
+++ demo_demos/inference.py
@@ -203,7 +203,7 @@ def random_check():
     print(sample)
 
 if __name__ == "__main__":
-    image = Image.open("./demo_demos/images/base.jpg").convert('RGBA')
+    image = Image.open("./images/base.jpg").convert('RGBA')
     ckpt_dir = './demo_demos/model'
     base_model = "runwayml/stable-diffusion-v1-5"
     dtype = torch.float16
